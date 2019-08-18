fa9afca26160c2d85e5b17d25f46ad081445cad7
moczniak <moczniak@gmail.com>
2019-08-18 12:33:02 +0200

    add types

---
+++ dist/index.d.ts
@@ -0,0 +1,17 @@
+declare type ClassConstructor = any;
+interface TestProviders {
+    provide: ClassConstructor;
+    useClass: ClassConstructor;
+}
+export declare class DiResolver {
+    static registerProvider(ctor: Function, deps: Function[]): void;
+    static registerSingleton(ctor: Function, deps: Function[]): void;
+    static registerLazySingleton(ctor: Function, deps: Function[]): void;
+    static resolve<T>(className: Function): T;
+    static resolveTesting<T>(className: Function, providers?: TestProviders[]): T;
+    static registerClassAsSingleton(instance: any): void;
+}
+export declare function Provide(deps?: Function[]): (ctor: Function) => void;
+export declare function Singleton(deps?: Function[]): (ctor: Function) => void;
+export declare function LazySingleton(deps?: Function[]): (ctor: Function) => void;
+export {};
